d02f8b99aff3d332d13dd079e858dee17a3260d1
BEcraft <becraftmcpe@gmail.com>
2017-06-23 12:50:58 -0400

    Added new styles

--- src/BEcraft/Tasks/Tornado.php
+++ src/BEcraft/Tasks/Tornado.php
@@ -5,9 +5,9 @@ namespace BEcraft\Tasks;
 use BEcraft\Loader;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\level\{Location, Level};
-use pocketmine\level\particle\{FlameParticle, WaterDripParticle};
+use pocketmine\level\particle\FlameParticle;
 
-class DoubleHelix extends PluginTask{
+class Tornado extends PluginTask{
 	
 	public function __construct(Loader $plugin, Location $location, Level $level){
 	parent::__construct($plugin, $location, $level);
@@ -19,21 +19,19 @@ class DoubleHelix extends PluginTask{
 	public function onRun($tick){
 	$level = $this->level;
 	$location = $this->location;
-	$radio = 5;
-	for($i = 5; $i > 0; $i-=0.1){
-	$radio = $i/3;
-	$x = $radio*cos(3*$i);
-	$y = 5-$i;
-	$z = $radio*sin(3*$i);
+	$pi = 3.14159;
+	$altura = 10;
+	$radio = 10;
+	$lineas = 4;
+	$upaltura = 0.2;
+	$upradio = $radio/$altura;
+	for($i = 0; $i < $lineas; ++$i){
+	for($y = 0; $y < $altura; $y+=$upaltura){
+	$radio = $y*$upradio;
+	$x = cos(deg2rad(360/$lineas*$i+$y*25))*$radio;
+	$z = sin(deg2rad(360/$lineas*$i+$y*25))*$radio;
 	$level->addParticle(new FlameParticle($location->add($x, $y, $z)));
 	}
-	for($i = 5; $i > 0; $i-=0.1){
-	$radio = $i/3;
-	$x = -$radio*cos(3*$i);
-	$y = 5-$i;
-	$z = -$radio*sin(3*$i);
-	$level->addParticle(new FlameParticle($location->add($x, $y, $z)));
 	}
 	}
-	
 }
